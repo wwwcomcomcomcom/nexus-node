520e915ccbdf4da670aba580c816aa9b1798102b
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-08 17:16:26 +0900

    Feat - socket.io chat demo

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import express from 'express';
+import http from 'http';
+import { Server, Socket } from 'socket.io';
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(port, () => {
+io.on('connection', (socket: Socket) => {
+    console.log('A user connected');
+
+    socket.on('chat message', (msg: string) => {
+        console.log('Message:', msg);
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
