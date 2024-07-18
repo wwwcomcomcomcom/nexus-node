2da1dfc668b50240f10c576337f2f8b7cb2a2ba6
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-18 17:28:42 +0900

    impl jwt service

--- src/jwt/jwtService.ts
+++ src/jwt/jwtService.ts
@@ -1,5 +1,6 @@
+import * as jwt from "jsonwebtoken";
 class JwtSerivce {
-  secretKey = process.env.JWT_SECRET;
+  secretKey: string;
 
   constructor(secretKey?: string) {
     if (secretKey) this.secretKey = secretKey;
@@ -7,7 +8,18 @@ class JwtSerivce {
     else throw new Error("Secret key is missing");
   }
 
-  parse(jwtToken: string): string {
-    throw new Error("Not implemented");
+  parse(jwtToken: string): { [keyof: string]: unknown } {
+    try {
+      const payload = jwt.verify(jwtToken, this.secretKey!, {
+        clockTimestamp: this.javaTimeStamp(),
+      });
+      return JSON.parse(payload.toString());
+    } catch (error) {
+      throw new Error("Invalid or expired token");
+    }
+  }
+
+  javaTimeStamp(): number {
+    return Math.floor(Date.now() / 1000);
   }
 }
