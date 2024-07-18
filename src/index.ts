ebbc1d20515c2afa289c93fb584b04fbaa72638d
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-18 09:19:00 +0900

    use chat service

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,27 @@
-import express from 'express';
-import http from 'http';
-import { Server, Socket } from 'socket.io';
+import express from "express";
+import http from "http";
+import { Server, Socket } from "socket.io";
+import mongoose from "mongoose";
+import { config } from "dotenv";
 
+//load config
+config();
+
+//Server Setting
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+//DB Setting
+mongoose.connect("mongodb://localhost:27017/chat");
+const db = mongoose.connection;
+db.on("error", (error) => console.log("connection failed ", error));
+db.once("open", () => console.log("Connected to MongoDB"));
 
-interface ChatMessageDto {
-    room: string;
-    message: string;
-}
-
-io.on('connection', (socket: Socket) => {
-    console.log('A user connected');
-
-    socket.on('chat message', (chatRequest: ChatMessageDto) => {
-        io.to(chatRequest.room).emit('chat message', chatRequest.message);
-    });
-    socket.on("join room", (room: string) => {
-        socket.join(room);
-    });
-    socket.on("leave room", (room: string) => {
-        socket.leave(room);
-    });
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
